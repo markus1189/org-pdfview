26519f603fcf5ec6ab1f034e30cd90f1d6b38550
Steve Purcell <steve@sanityinc.com>
2015-02-14 11:59:07 +0000

    Describe the recommended method for enabling org-pdfview

--- org-pdfview.el
+++ org-pdfview.el
@@ -23,6 +23,9 @@
 
 ;;; Commentary:
 ;; Add support for org links from pdfview buffers like docview.
+;;
+;; To enable this automatically, use:
+;;     (eval-after-load 'org '(require 'org-pdfview))
 
 ;;; Code:
 (require 'org)
